909bac5e899845c62044e088c77db7dc5ab0aa38
Icelk <main@icelk.dev>
2020-11-16 15:21:31 +0100

    Fixed write example. Now discards header.

--- tinyquest/examples/write/main.rs
+++ tinyquest/examples/write/main.rs
@@ -1,7 +1,8 @@
 use std::fs::File;
 
 fn main() {
-    tinyquest::get("alligator.io", "My app")
+    let request = http::Request::get("alligator.io").body(Vec::new()).unwrap();
+    tinyquest::request(request, tinyquest::Config::no_header())
         .unwrap()
         .follow_redirects_write(&mut File::create("example.html").unwrap())
         .unwrap()
